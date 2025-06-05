37c0612ca863827b152dbbb908b8fd87a158f2d9
Or Antebi <or87600@gmail.com>
2025-06-05 20:58:29 +0300

    fix: add placeholderText prop to SearchInput component

--- src/cmps/SearchInput.jsx
+++ src/cmps/SearchInput.jsx
@@ -1,12 +1,12 @@
 import submitIcon from '../assets/img/submit-icon.svg'
 
-export default function SearchInput({ submitBtn = true }) {
+export default function SearchInput({ submitBtn = true, placeholderText = "What service are you looking for today?" }) {
     return (
         <form className="search-bar-container flex justify-between">
             <input
                 type="search"
                 className={`search-bar ${!submitBtn ? "no-submit-btn" : ""}`}
-                placeholder="What service are you looking for today?"
+                placeholder={placeholderText}
             />
             {submitBtn && (
                 <button type="submit" className="search-btn">
