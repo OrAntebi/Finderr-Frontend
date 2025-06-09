84135c5db464a9e625ae6d38a7b6756110a3a72b
Or Antebi <or87600@gmail.com>
2025-06-09 16:24:09 +0300

    Refactor to GigList + GigPreview and add owner rating to GigDetails

--- src/cmps/GigPreview.jsx
+++ src/cmps/GigPreview.jsx
@@ -1,7 +1,12 @@
+import { gigservice } from "../services/gig"
 import { Link } from "react-router-dom"
 
 export function GigPreview({ gig }) {
-    const mainImg = 'src/assets/img/gigImg.jpg'
+
+    const levelStars = gigservice.convertLvlToStars(gig.owner.level)
+        .map((src, idx) => (
+            <img key={idx} src={src} alt="star" className="star-img" />
+        ))
 
 
     return (
@@ -9,24 +14,29 @@ export function GigPreview({ gig }) {
             {/* <img className="gig-img" src={mainImg} alt={gig.title} /> */}
             <img className="gig-img" src={gig.imgUrls[0]} alt={gig.title} />
 
-            <div className="owner-row">
-                <img className="owner-avatar" src={'/src/assets/img/ownerImg.jpg'} />
-                {/* <img className="owner-avatar" src={`https://api.dicebear.com/8.x/bottts/png?seed=${(gig.title)}`} /> */}
+            <div className="owner-row flex align-center justify-between">
+                <div className="owner-details flex align-center">
+                    <img className="owner-avatar" src={'/src/assets/img/ownerImg.jpg'} />
+                    {/* <img className="owner-avatar" src={`https://api.dicebear.com/8.x/bottts/png?seed=${(gig.title)}`} /> */}
+                    <span className="owner-name">{gig.owner.fullname}</span>
+                </div>
 
-                <span className="owner-name">{gig.owner.fullname}</span>
+                <span className="owner-level flex align-center">
+                    <p>Level {gig.owner.level}</p>
+                    <span className="stars flex align-center">{levelStars}</span>
+                </span>
             </div>
 
             <p className="gig-title">{gig.title}</p>
 
-            <div className="rating-row">
+            <div className="rating-row flex align-center">
                 <span className="star">★</span>
                 <span className="rate-num">{gig.owner.rate.toFixed(1)}</span>
                 <span className="rate-count">({gig.reviews})</span>
             </div>
 
-            <div className="price-row">
-                <span>From </span>
-                <span className="price">${gig.price}</span>
+            <div className="price-row flex align-center">
+                <span className="price">From ${gig.price}</span>
             </div>
         </Link>
     )
--- src/services/gig/index.js
+++ src/services/gig/index.js
@@ -1,33 +1,41 @@
 const { DEV, VITE_LOCAL } = import.meta.env
 import { getRandomIntInclusive, makeId } from '../util.service'
+import fullStar from '../../assets/img/owner-level-full.svg'
+import emptyStar from '../../assets/img/owner-level-empty.svg'
 
 import { gigservice as local } from './gig.service.local'
 import { gigservice as remote } from './gig.service.remote'
 
 function getEmptyGig() {
-    return {
-        vendor: makeId(),
-        price: getRandomIntInclusive(1000, 9000),
-        speed: getRandomIntInclusive(80, 240),
-        msgs: [],
-    }
+  return {
+    vendor: makeId(),
+    price: getRandomIntInclusive(1000, 9000),
+    speed: getRandomIntInclusive(80, 240),
+    msgs: [],
+  }
 }
 
 function getDefaultFilter() {
-    return {
-        txt: '',
-        maxPrice: '',
-        minSpeed: '',
-        sortField: '',
-        sortDir: '',
-        // pageIdx: 0
-    }
+  return {
+    txt: '',
+    maxPrice: '',
+    minSpeed: '',
+    sortField: '',
+    sortDir: '',
+    // pageIdx: 0
+  }
 }
 
-// console.log('VITE_LOCAL:', VITE_LOCAL)
+function convertLvlToStars(level) {
+  const maxStars = 3
+
+  return Array.from({ length: maxStars }, (_, i) =>
+    i < level ? fullStar : emptyStar
+  )
+}
 
 const service = VITE_LOCAL === 'true' ? local : remote
-export const gigservice = { getEmptyGig, getDefaultFilter, ...service }
+export const gigservice = { getEmptyGig, getDefaultFilter, convertLvlToStars, ...service }
 
 
 
