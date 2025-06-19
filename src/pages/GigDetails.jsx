ab3cb25aad1087a0e8688e3f9d517a6477b7be0d
ShayIsso <shayisso@gmail.com>
2025-06-19 22:16:26 +0300

    Refactor modal structure in GigDetails; enhance modal styling and transition effects

--- src/pages/GigDetails.jsx
+++ src/pages/GigDetails.jsx
@@ -88,65 +88,63 @@ export function GigDetails() {
                     onSelectPackage={setSelectedPackage}
                 />)
             }
-            {isModalOpen &&
-                <dialog className="modal-payment flex column" open>
-                    <div className="modal-title flex align-center justify-between">
-                        <span>Order options</span>
-                        <button className="close-modal-btn" onClick={() => setIsModalOpen(false)}>
-                            <img src={icons.close} alt="close-icon" />
-                        </button>
-                    </div>
-                    <div className="modal-details-container">
-                        <div className="modal-description flex column">
-                            <li>
-                                <div className="package-and-price flex justify-between">
-                                    <p className="current-package">{selectedPackage.charAt(0).toUpperCase() + selectedPackage.slice(1)}</p>
-                                    <p className="amount">${selectedPack.packPrice}</p>
-                                </div>
-                                <div className="gig-description-text">
-                                    <span>{selectedPackage.charAt(0).toUpperCase() + selectedPackage.slice(1)} </span>
-                                    <span>{gig.title}</span>
-                                </div>
-                            </li>
-                        </div>
-                        <div className="modal-price">
-                            <div className="price-container flex column">
-                                <span>${selectedPack.packPrice}</span>
-                                <span>Single order</span>
+            <div className={`modal-payment flex column ${isModalOpen ? 'modal-open' : ''}`}>
+                <div className="modal-title flex align-center justify-between">
+                    <span>Order options</span>
+                    <button className="close-modal-btn" onClick={() => setIsModalOpen(false)}>
+                        <img src={icons.close} alt="close-icon" />
+                    </button>
+                </div>
+                <div className="modal-details-container">
+                    <div className="modal-description flex column">
+                        <li>
+                            <div className="package-and-price flex justify-between">
+                                <p className="current-package">{selectedPackage.charAt(0).toUpperCase() + selectedPackage.slice(1)}</p>
+                                <p className="amount">${selectedPack.packPrice}</p>
                             </div>
-                            <ul className="modal-delivery-info flex column">
-                                <div className="package-type flex align-center">
-                                    <span className="package flex align-center"><img src={icons.package} alt="package-icon" /></span>
-                                    <span>{selectedPackage.charAt(0).toUpperCase() + selectedPackage.slice(1)} package</span>
-                                </div>
-                                <div className="delivery-time flex align-center">
-                                    <span><img src={icons.clock} alt="clock-icon" /></span>
-                                    <span>{selectedPack.packDaysToMake}-day delivery</span>
-                                </div>
-                                <div className="revisions flex align-center">
-                                    <span><img src={icons.round} alt="round-icon" /></span>
-                                    <span>
-                                        {selectedPackage === 'premium'
-                                            ? 'Unlimited Revisions'
-                                            : selectedPackage === 'standard'
-                                                ? '3 Revisions'
-                                                : '1 Revision'}
-                                    </span>
-                                </div>
-                            </ul>
-                        </div>
+                            <div className="gig-description-text">
+                                <span>{selectedPackage.charAt(0).toUpperCase() + selectedPackage.slice(1)} </span>
+                                <span>{gig.title}</span>
+                            </div>
+                        </li>
                     </div>
-                    <div className="modal-footer">
-                        <button className="continue-btn flex align-center justify-center">
-                            <span>Continue</span>
-                            <span>(${selectedPack.packPrice})</span>
-                        </button>
-                        <div className="flex align-center justify-center">
-                            <span>You won't be charged yet</span>
+                    <div className="modal-price">
+                        <div className="price-container flex column">
+                            <span>${selectedPack.packPrice}</span>
+                            <span>Single order</span>
                         </div>
+                        <ul className="modal-delivery-info flex column">
+                            <div className="package-type flex align-center">
+                                <span className="package flex align-center"><img src={icons.package} alt="package-icon" /></span>
+                                <span>{selectedPackage.charAt(0).toUpperCase() + selectedPackage.slice(1)} package</span>
+                            </div>
+                            <div className="delivery-time flex align-center">
+                                <span><img src={icons.clock} alt="clock-icon" /></span>
+                                <span>{selectedPack.packDaysToMake}-day delivery</span>
+                            </div>
+                            <div className="revisions flex align-center">
+                                <span><img src={icons.round} alt="round-icon" /></span>
+                                <span>
+                                    {selectedPackage === 'premium'
+                                        ? 'Unlimited Revisions'
+                                        : selectedPackage === 'standard'
+                                            ? '3 Revisions'
+                                            : '1 Revision'}
+                                </span>
+                            </div>
+                        </ul>
                     </div>
-                </dialog>
-            }
+                </div>
+                <div className="modal-footer">
+                    <button className="continue-btn flex align-center justify-center">
+                        <span>Continue</span>
+                        <span>(${selectedPack.packPrice})</span>
+                    </button>
+                    <div className="flex align-center justify-center">
+                        <span>You won't be charged yet</span>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
